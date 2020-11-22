bc53fa8fe8bcc08aea739017eac376b532c4a66a
Lucsvieirabr <62450294+Lucsvieirabr@users.noreply.github.com>
2020-11-21 23:08:18 -0300

    Change Background type, Menu and GearButton

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+var url;
+
 function getBackgroundAndChampNameFromAPI(){
     var requisicao = new Request("https://awesome-api.vercel.app/api/lol")
     fetch(requisicao).then(resposta => resposta.json()).then(json =>{
-        var url = json.img;
+        url = json.img;
         var name = json.name;
         document.getElementById('html').style.backgroundImage= "url(" + url +')';
         document.getElementById('html').style.backgroundSize='cover';
@@ -11,3 +13,42 @@ function getBackgroundAndChampNameFromAPI(){
     })
 }
 getBackgroundAndChampNameFromAPI();
+
+    
+function OpenMenu(){
+
+
+    document.getElementById('menu').style.setProperty('display', 'block');
+
+
+}
+
+
+function changeBackgroundType(){
+
+    var backtype = document.getElementById("backtype").value;
+    
+    if(backtype === "repeat"){
+  
+        document.getElementById('html').style.backgroundSize='auto';
+
+        
+    }
+
+    if(backtype === "cover"){
+        
+        document.getElementById('html').style.backgroundSize='cover';
+
+        
+    }
+
+    if(backtype === "diagonal"){
+
+        
+        document.getElementById('html').style.backgroundSize='auto';
+        
+        
+    }
+
+
+}
